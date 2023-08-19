b6b58cfdc70eb12a5ee829162c123e46c4dc87d2
EnriqueRS <enrique.rios.santos@gmail.com>
2023-08-19 18:31:09 +0200

    add actuator. add test property endpoint.

--- todo-service/src/main/java/com/enriquers/todo/controller/ToDoController.java
+++ todo-service/src/main/java/com/enriquers/todo/controller/ToDoController.java
@@ -4,7 +4,10 @@ import com.enriquers.todo.model.ToDo;
 import com.enriquers.todo.repository.TodoRepository;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,11 +16,20 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor(onConstructor = @__(@Autowired))
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@RefreshScope
 @RequestMapping("api/v1/todos")
 public class ToDoController {
 
   private final TodoRepository todoRepository;
+
+  @Value("${todo.test}")
+  private String test;
+
+  @GetMapping("/test-property")
+  public String testProperty() {
+    return test;
+  }
 
   @GetMapping("")
   @ResponseStatus(HttpStatus.OK)
