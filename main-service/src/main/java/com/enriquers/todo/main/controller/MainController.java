e70e83033ae9687cf1664c1654aecfedde1d31da
EnriqueRS <enrique.rios.santos@gmail.com>
2023-09-24 16:47:19 +0200

    gateway added

--- main-service/src/main/java/com/enriquers/todo/main/controller/MainController.java
+++ main-service/src/main/java/com/enriquers/todo/main/controller/MainController.java
@@ -1,11 +1,19 @@
 package com.enriquers.todo.main.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-@RestController
+@RestController("/api/v1/todo")
 @RequiredArgsConstructor
 public class MainController {
+
+  @GetMapping("/{todoId}")
+  @ResponseStatus(HttpStatus.OK)
+  public String getToDo() {
+    return "Hello World";
+  }
 
 }
