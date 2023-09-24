aeab3d359a5e25cfac25b6b44a6a72ee51058245
EnriqueRS <enrique.rios.santos@gmail.com>
2023-09-24 18:23:48 +0200

    fix main service

--- main-service/src/main/java/com/enriquers/todo/main/controller/MainController.java
+++ main-service/src/main/java/com/enriquers/todo/main/controller/MainController.java
@@ -3,17 +3,20 @@ package com.enriquers.todo.main.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1/todo")
+@RestController
+@RequestMapping("/api/v1/todo")
 @RequiredArgsConstructor
 public class MainController {
 
   @GetMapping("/{todoId}")
   @ResponseStatus(HttpStatus.OK)
-  public String getToDo() {
-    return "Hello World";
+  public String getToDo(@PathVariable String todoId) {
+    return "Hello World " + todoId;
   }
 
 }
