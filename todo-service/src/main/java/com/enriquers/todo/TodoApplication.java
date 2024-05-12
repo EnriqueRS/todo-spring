59dc1be095deed0c1e93065fb1949162e8ac16d7
EnriqueRS <enrique.rios.santos@gmail.com>
2024-05-12 21:53:00 +0200

    eureka config updated

--- todo-service/src/main/java/com/enriquers/todo/TodoApplication.java
+++ todo-service/src/main/java/com/enriquers/todo/TodoApplication.java
@@ -2,8 +2,10 @@ package com.enriquers.todo;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TodoApplication {
 
 	public static void main(String[] args) {
